177198d6d68d2dceaa52359b80d1f898d7076116
Alexander Claes <hello@aclaes.com>
2022-04-07 16:36:41 +0200

    Update bookmark

--- components/bookmark/bookmark.js
+++ components/bookmark/bookmark.js
@@ -1,5 +1,6 @@
 export default function Bookmark(bookmarkElement) {
   bookmarkElement.addEventListener('click', () => {
+    console.log('test');
     bookmarkElement.classList.toggle('bookmark--active');
   });
 }
